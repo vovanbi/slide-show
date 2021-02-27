1e68d8e28457ad072241bf5b0f2d21564157a234
vovanbi <vanbi010599@gmail.com>
2021-02-27 16:10:08 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import React, { Component } from 'react'
+import anh1 from './img/1.jpg'
+import anh2 from './img/2.jpg'
+import anh3 from './img/3.jpg'
+import anh4 from './img/4.jpg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const listImage = [
+  {
+    title: 'Anh1',
+    src: anh1
+  },
+  {
+    title: 'Anh2',
+    src: anh2
+  },
+  {
+    title: 'Anh3',
+    src: anh3
+  },
+  {
+    title: 'Anh4',
+    src: anh4
+  }
+]
+
+let timeout
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      index: 0
+    }
+  }
+  componentDidUpdate() {
+    if (timeout) clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      this.setState(oldState => ({
+        index: oldState.index + 1 >= listImage.length ? 0 : oldState.index + 1
+      }))
+    }, 5000)
+  }
+  componentDidMount() {
+    if (timeout) clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      this.setState(oldState => ({
+        index: oldState.index + 1 >= listImage.length ? 0 : oldState.index + 1
+      }))
+    }, 5000)
+  }
+  handleNextClick = () => {
+    if (timeout) clearTimeout(timeout)
+    this.setState(oldState => ({
+      index: oldState.index + 1 >= listImage.length ? 0 : oldState.index + 1
+    }))
+  }
+
+  handlePreClick = () => {
+    if (timeout) clearTimeout(timeout)
+    this.setState(oldState => ({
+      index: oldState.index - 1 < 0 ? listImage.length - 1 : oldState.index - 1
+    }))
+  }
+
+  render() {
+    return (
+      <div className='lp-slideshow-gallery'>
+        <div className='container'>
+          <button className='action-button pre' onClick={this.handlePreClick}>&#10094;</button>
+          <button className='action-button next' onClick={this.handleNextClick}>&#10095;</button>
+          {listImage.map((item, idx) => (
+            <div
+              key={idx}
+              className={
+                `slide ${idx === this.state.index ? 'active' : ''}`
+              }
+            >
+              <div className='number-text'>
+                {`${this.state.index + 1} / ${listImage.length}`}
+              </div>
+              <img alt={item.title} src={item.src} className='image' />
+              <div className='caption-text'>{item.title}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
